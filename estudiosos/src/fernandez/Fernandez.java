d71ee9ed38948f316f5fb77701aec4e0225acef3
Raen98 <david.fernandez1998.rd@gmail.com>
2024-05-07 20:14:46 +0200

    package fernandez

---
+++ estudiosos/src/fernandez/Fernandez.java
@@ -0,0 +1,13 @@
+package fernandez;
+
+public class Fernandez {
+
+
+		public static void main(String[] args) {
+			hola();
+		}
+		
+		private static void hola(){
+			System.out.println("Hola, me llamo David Fernandez");
+		}
+	}
