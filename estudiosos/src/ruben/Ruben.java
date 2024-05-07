03d23076158d361b168287d61bd0169b65144a56
raiben2123 <rubenmrtnz02@gmail.com>
2024-05-07 20:16:33 +0200

    package ruben

---
+++ estudiosos/src/ruben/Ruben.java
@@ -0,0 +1,13 @@
+package ruben;
+
+public class Ruben {
+
+	public static void main(String[] args) {
+		
+		hola();
+	}
+	
+	private static void hola() {
+		System.out.println("Hola, soy Rubén");
+	}
+}
